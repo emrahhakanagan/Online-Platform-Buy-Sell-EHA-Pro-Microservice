8c8d5ebd228437d65c4e617549d7b6b233c33e60
Emrah Hakan AGAN <134237010+emrahhakanagan@users.noreply.github.com>
2024-11-15 00:16:00 +0300

    chore: initialize Spring Boot project with Maven and essential dependencies

---
+++ src/main/java/com/ehapro/buyandsellehapromicroservice/OnlineBuySellPlatformEhaProApplication.java
@@ -0,0 +1,13 @@
+package com.ehapro.buyandsellehapromicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineBuySellPlatformEhaProApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineBuySellPlatformEhaProApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ehapro/buyandsellehapromicroservice/OnlineBuySellPlatformEhaProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ehapro.buyandsellehapromicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineBuySellPlatformEhaProApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
